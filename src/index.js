be4655eeb232753589da66f2bef8fe2fcbff1487
Vincent Chow <v123456k212561256@gmail.com>
2021-04-25 12:34:20 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.render(
-  <BrowserRouter basename="/PersonalWebsite">
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
