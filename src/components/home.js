2b3d713c8fd607c7bc2c0c81b9d9a41f432736c5
Vincent Chow <v123456k212561256@gmail.com>
2021-04-25 12:40:52 +0800

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -5,15 +5,11 @@ export default class Home extends Component {
     render() {
       return (
         <section id = "mainContent">
-            <div className = "mainContent">
-                  <div style={{backgroundImage: 'url(/PersonalWebsite/images/about.jpg)'}} height='100%' width = "100%"> 
-                    <h1 className = "text">Hi There! <br/> I'm Vincent</h1>
-                    <h2 className = "textRight">| Software Engineer | <br/> | World Traveller |<br/>| Food Connoisseur |</h2>
-                    <h5 className = "text2"> Interested? <br/> then keep reading on</h5>
-                  </div>
-                  
-            </div>
-                               
+          <div className = "homeImg" style={{backgroundImage: 'url(/PersonalWebsite/images/home.png)'}}> 
+            <h1 className = "text">Hi There! <br/> I'm Vincent</h1>
+            <h2 className = "textRight">| Software Engineer | <br/> | World Traveller |<br/>| Food Connoisseur |</h2>
+            <h5 className = "text2"> Interested? <br/> then keep reading on</h5>
+          </div>                           
         </section>
         
       )
