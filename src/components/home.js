51339ab9f162e9248b21ab6189649549deb4a827
Vincent Chow <v123456k212561256@gmail.com>
2021-04-25 11:52:30 +0800

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -8,7 +8,12 @@ export default class Home extends Component {
         <section id = "mainContent">
             <div className = "mainContent">
                 <div className = "homeImg">
-                  <div style={{backgroundImage: `url(${homeImg})`}}> 
+                  <img
+                    className="d-block w-100 "
+                    style={{ backgroundImage: `url(${homeImg})` }}
+                    alt=""
+                  />
+                  <div> 
                     <h1 className = "text">Hi There! <br/> I'm Vincent</h1>
                     <h2 className = "textRight">| Software Engineer | <br/> | World Traveller |<br/>| Food Connoisseur |</h2>
                     <h5 className = "text2"> Interested? <br/> then keep reading on</h5>
