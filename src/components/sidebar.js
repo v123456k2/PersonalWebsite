49d41588757ad4e59fb9b2ae54d5a5aa3da3541c
Vincent Chow <v123456k212561256@gmail.com>
2021-04-25 11:34:42 +0800

    test img

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -17,7 +17,7 @@ const Sidebar = () => {
     <div>
         <section id ="sidebar">
             <div className = "sidebar">
-              <div className = "author-img" style ={{backgroundImage: aboutImg}} />
+              <div className = "author-img" style ={{backgroundImage: 'url(/PersonalWebsite/images/about.jpg)'}} />
               <h1 className = "name"> Vincent Chow </h1>
               <span className="mail"><i className="fa fa-envelope"></i> v123456k212561256@gmail.com</span>
               <span className="phone"><i className="fa fa-phone"></i> +1 724-328-2719</span>
