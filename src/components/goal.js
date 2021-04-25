40e2b15078de94a537b7b15497696f43aaf07666
Vincent Chow <v123456k212561256@gmail.com>
2021-04-25 15:16:35 +0800

    goal format

--- src/components/goal.js
+++ src/components/goal.js
@@ -5,11 +5,9 @@ import homeImg from "../assets/images/home.png";
 export default class Goal extends Component {
     render() {
       return (
-        <section id = "mainContent">
           <div  className = "careerimg">
             <img src = '/PersonalWebsite/images/careergoals.png'></img>
             </div>
-        </section>
         
       )
     }
